47a810f6f504a97d6f1b9da6a1c3b7dece5146e1
Jonsterry <98904340+Jonsterry@users.noreply.github.com>
2022-02-09 00:52:26 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -6,5 +6,8 @@ y = 3
 
 
 resultado = z + x * 365 - y * 52
-(20)+(10*365)-(3*52)=3514
+
+monedas_encontradas=20
+monedas_magicas=10
+monedas_robadas=3
 (monedas_encontradas)+(monedas_magicas*365)-(monedas_robadas*52)=3514
